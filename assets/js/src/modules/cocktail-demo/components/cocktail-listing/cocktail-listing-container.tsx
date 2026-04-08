5f2843cab33a581c2c2d8dcded13c3070cfce476
Marvin Bertram <marvin.bertram@pimcore.com>
2026-04-08 11:55:30 +0000

    Add missing element context provider

--- assets/js/src/modules/cocktail-demo/components/cocktail-listing/cocktail-listing-container.tsx
+++ assets/js/src/modules/cocktail-demo/components/cocktail-listing/cocktail-listing-container.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useInjection } from '@pimcore/studio-ui-bundle/app'
-import { BaseListing, listingDefaultProps } from '@pimcore/studio-ui-bundle/modules/data-object'
+import { BaseListing, listingDefaultProps, DataObjectProvider } from '@pimcore/studio-ui-bundle/modules/data-object'
 import { type ListingBuilder } from '@pimcore/studio-ui-bundle/modules/element'
 
 const COCKTAIL_CLASS_NAME = 'Cocktail'
+const PIMCORE_ROOT_ID = 1
 
 export const CocktailListingContainer = (): React.JSX.Element => {
   const listingBuilder = useInjection<ListingBuilder>(
@@ -11,19 +12,20 @@ export const CocktailListingContainer = (): React.JSX.Element => {
   )
 
   return (
-    <BaseListing
-      { ...listingBuilder.build({
-        props: listingDefaultProps,
-        config: {
-          classDefinitionSelection: {
-            config: {
-              classRestriction: [{ classes: COCKTAIL_CLASS_NAME }],
-              showConfigLayer: false,
-              isResolvingClassDefinitionsBasedOnElementId: false
+    <DataObjectProvider id={ PIMCORE_ROOT_ID }>
+      <BaseListing
+        { ...listingBuilder.build({
+          props: listingDefaultProps,
+          config: {
+            classDefinitionSelection: {
+              config: {
+                classRestriction: [{ classes: COCKTAIL_CLASS_NAME }],
+                showConfigLayer: false
+              }
             }
           }
-        }
-      }) }
-    />
+        }) }
+      />
+    </DataObjectProvider>
   )
 }
