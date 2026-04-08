0e168c6bfd9d091d5fd51249249fd3a654d0b8ca
Martin Eiber <martin.eiber@pimcore.com>
2026-04-08 14:12:35 +0200

    Basic Setup

--- src/DependencyInjection/PimcoreInspireCocktailDemoExtension.php
+++ src/DependencyInjection/PimcoreInspireCocktailDemoExtension.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Pimcore\Bundle\InspireCocktailDemoBundle\DependencyInjection;
@@ -7,13 +6,21 @@ namespace Pimcore\Bundle\InspireCocktailDemoBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class PimcoreInspireCocktailDemoExtension extends Extension
+class PimcoreInspireCocktailDemoExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
+        $loader->load('studio_backend.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('pimcore/studio_backend.yaml');
     }
 }
---
+++ src/OpenApi/Config/Tags.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Pimcore\Bundle\InspireCocktailDemoBundle\OpenApi\Config;
+
+use OpenApi\Attributes\Tag;
+
+#[Tag(
+    name: Tags::InspireCocktailDemo->value,
+    description: 'bundle_tag_inspire_cocktail_demo_description',
+)]
+/**
+ * @internal
+ */
+enum Tags: string
+{
+    case InspireCocktailDemo = 'Bundle Inspire Cocktail Demo';
+}
